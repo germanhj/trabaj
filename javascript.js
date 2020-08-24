afe013c9e1865211720bb920da59a35f51eaa683
perrito <perrito@gmail.com>
2020-08-24 03:02:58 -0300

    hola trabajo

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let dog = {
+  name: 'doki',
+  age: 11,
+  vacuna: true,
+  colors: 'yellow'
+}
+// Recorrer objetos
+Object.entries() => // Devuelve como array cada una de las entradas del objeto(propiedad,valor)
+Object.Keys() => // Devuelve un object (array like)con toda las propiedades
+Object.Values() => // Devuelve los valores
